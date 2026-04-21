5cccbd02b37926a8c48b07ed956edbf29e780ebc
Shutman-ZTAY <ismaelestanislaoc@gmail.com>
2026-04-21 01:00:53 -0400

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,7 @@ Test cases can be run with the following:
 """
 import os
 import logging
+from service import talisman
 from unittest import TestCase
 from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
@@ -35,6 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
